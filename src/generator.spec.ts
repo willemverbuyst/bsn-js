490fe7c011f5ee2c78b72de44d7336e18f7d82e8
Willem Verbuyst <willemverbuyst@gmail.com>
2026-03-15 10:16:08 +0100

    Replace jest with vitest

--- src/generator.spec.ts
+++ src/generator.spec.ts
@@ -1,3 +1,4 @@
+import { describe, test, expect } from "vitest";
 import { generateBSN } from "./generator";
 import { isValidBSN } from "./validator";
 
--- src/validator.spec.ts
+++ src/validator.spec.ts
@@ -1,3 +1,4 @@
+import { describe, test, expect } from "vitest";
 import { isValidBSN } from "./validator";
 
 const validBSNs = [
