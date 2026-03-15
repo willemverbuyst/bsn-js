fe4e6da6367c945c6e08e3a0e599cc5ea7b95c2c
Willem Verbuyst <willemverbuyst@gmail.com>
2026-03-15 10:03:27 +0100

    Update code checks

--- src/generator.spec.ts
+++ src/generator.spec.ts
@@ -1,15 +1,14 @@
-import { generateBSN } from './generator'
-import { isValidBSN } from './validator'
+import { generateBSN } from "./generator";
+import { isValidBSN } from "./validator";
 
-describe('#generateBSN', () => {
-	const testcases = Array.from({ length: 50 }, (_, i) => i + 1)
+describe("#generateBSN", () => {
+  const testcases = Array.from({ length: 50 }, (_, i) => i + 1);
 
-	test.each(testcases)('%s: should generate valid BSN)', () =>
-		expect(isValidBSN(generateBSN())).toBe(true)
-	)
+  test.each(testcases)("%s: should generate valid BSN)", () =>
+    expect(isValidBSN(generateBSN())).toBe(true),
+  );
 
-	test.each(testcases)(
-		'%s: with leading "0000" should generate valid BSN)',
-		() => expect(isValidBSN(generateBSN(true))).toBe(true)
-	)
-})
+  test.each(testcases)('%s: with leading "0000" should generate valid BSN)', () =>
+    expect(isValidBSN(generateBSN(true))).toBe(true),
+  );
+});
